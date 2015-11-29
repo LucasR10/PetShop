0acc7c0edbf0129f43370a84eaa98e8300087270
Ronaldo Baptist <Ronaldo Baptist@RonaldoBatista>
2015-11-29 16:20:41 -0200

    commit cliente.

---
+++ src/br/com/petshop/modelo/Cliente.java
@@ -0,0 +1,16 @@
+package br.com.petshop.modelo;
+
+/**
+ * @author Ronaldo 
+ * 
+ * @since 29/11/2015
+ * 
+ * version 1.0
+ *  
+ * */
+public class Cliente {
+	int idCliente;
+	String nome;
+	String cpf;
+	
+}
