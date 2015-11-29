67542eaced8257cc4588747b6fd8eb26cf462cfb
Lucas Rocha <LucasR10@users.noreply.github.com>
2015-11-29 16:08:30 -0200

    commit arquivos.

---
+++ src/br/com/petshop/modelo/Produto.java
@@ -0,0 +1,19 @@
+package br.com.petshop.modelo;
+
+
+/**
+ * @author Ronaldo
+ *  
+ * @since 29/11/205
+ * 
+ * @version 1.0
+ * 
+ * */
+
+
+public class Produto {
+	
+	int idProduto;
+    String nome;
+    String descricao;
+}
